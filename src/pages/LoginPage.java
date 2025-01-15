f398fc3766c6226c1e7bb158927ba75326b08cd6
AmritaDinda <amritadinda84@gmail.com>
2025-01-15 13:00:59 +0530

    added/updated/deleted files

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,5 +4,9 @@ public class LoginPage {
 
 	public static void main(String []a) {
 		System.out.println("hello LoginPage");
+		String us = "admin";
+		String pwd = "admin";
+		System.out.println("logged in .......");
+
 	}
 }
